e9b96cc1a740856a158dbc5f966cba944a3f94b2
19040971 <19040971@DESKTOP-2OPUF9T>
2020-07-22 17:46:47 +0800

    retrieveAllChromebookTest() updated

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -97,17 +97,17 @@ public class ResourceCentreTest {
 		// Test if Item list is not null but empty, so that can add a new item
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 				
-		//test if the list of camcorders retrieved from the SourceCentre is empty
+		//test if the list of chromebook retrieved from the SourceCentre is empty
 		String allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 		String testOutput = "";
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allChromebook);
+		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 						
 		//Given an empty list, after adding 2 items, test if the size of the list is 2
 		ResourceCentre.addChromebook(chromebookList, cb1);
 		ResourceCentre.addChromebook(chromebookList, cb2);
 		assertEquals("Test if that Chromebook arraylist size is 2?", 2, chromebookList.size());
 			
-		//test if the expected output string same as the list of camcorders retrieved from the SourceCentre
+		//test if the expected output string same as the list of chromebook retrieved from the SourceCentre
 		allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 
 		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CB0011", "ASUS Chromebook", "Yes", "", 40);
